180b8976a850e785ddbfb3c527ae75f1c0b47c4e
orlali <64191879+orlali@users.noreply.github.com>
2020-05-21 12:32:02 +1200

    Add project files.

---
+++ files_writeAndRead/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace files_writeAndRead
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWriteData = new System.Windows.Forms.Button();
+            this.txtDataToAdd = new System.Windows.Forms.TextBox();
+            this.btnReadData = new System.Windows.Forms.Button();
+            this.lblDisplayData = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnWriteData
+            // 
+            this.btnWriteData.Location = new System.Drawing.Point(238, 228);
+            this.btnWriteData.Name = "btnWriteData";
+            this.btnWriteData.Size = new System.Drawing.Size(139, 52);
+            this.btnWriteData.TabIndex = 0;
+            this.btnWriteData.Text = "add text to file";
+            this.btnWriteData.UseVisualStyleBackColor = true;
+            this.btnWriteData.Click += new System.EventHandler(this.btnWriteData_Click);
+            // 
+            // txtDataToAdd
+            // 
+            this.txtDataToAdd.Location = new System.Drawing.Point(238, 153);
+            this.txtDataToAdd.Name = "txtDataToAdd";
+            this.txtDataToAdd.Size = new System.Drawing.Size(148, 26);
+            this.txtDataToAdd.TabIndex = 1;
+            // 
+            // btnReadData
+            // 
+            this.btnReadData.Location = new System.Drawing.Point(530, 153);
+            this.btnReadData.Name = "btnReadData";
+            this.btnReadData.Size = new System.Drawing.Size(152, 48);
+            this.btnReadData.TabIndex = 2;
+            this.btnReadData.Text = "read text from file";
+            this.btnReadData.UseVisualStyleBackColor = true;
+            this.btnReadData.Click += new System.EventHandler(this.btnReadData_Click);
+            // 
+            // lblDisplayData
+            // 
+            this.lblDisplayData.AutoSize = true;
+            this.lblDisplayData.Location = new System.Drawing.Point(530, 228);
+            this.lblDisplayData.Name = "lblDisplayData";
+            this.lblDisplayData.Size = new System.Drawing.Size(132, 20);
+            this.lblDisplayData.TabIndex = 3;
+            this.lblDisplayData.Text = "This is in the file...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDisplayData);
+            this.Controls.Add(this.btnReadData);
+            this.Controls.Add(this.txtDataToAdd);
+            this.Controls.Add(this.btnWriteData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWriteData;
+        private System.Windows.Forms.TextBox txtDataToAdd;
+        private System.Windows.Forms.Button btnReadData;
+        private System.Windows.Forms.Label lblDisplayData;
+    }
+}
+
---
+++ files_writeAndRead/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace files_writeAndRead
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWriteData_Click(object sender, EventArgs e)
+        {
+            string textData = txtDataToAdd.Text;
+            //System.IO.File.WriteAllText(@"C:\13CSC\files_writeAndRead\textData.txt", textData);
+            
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(@"C:\13CSC\files_writeAndRead\textData.txt", true))
+            {
+                file.WriteLine(textData);
+            }
+            
+        }
+
+        private void btnReadData_Click(object sender, EventArgs e)
+        {
+            //string text = System.IO.File.ReadAllText(@"C:\13CSC\files_writeAndRead\textData.txt");
+            //lblDisplayData.Text = text;
+            
+            string[] lines = System.IO.File.ReadAllLines("testDebugFolderText.txt");
+            for (int i = 0; i < lines.Length; i++)
+            {
+                lblDisplayData.Text += "\n" + lines[i];
+            }
+
+
+        }
+    }
+}
